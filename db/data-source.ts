f0fe2e6ce0ee784d25d69bf0f3ade06d0ef69ef4
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-06-01 22:51:57 +0700

    fix bug

--- db/data-source.ts
+++ db/data-source.ts
@@ -8,6 +8,7 @@ import { Playlist } from 'src/playlists/playlists.entity';
 import { Song } from 'src/songs/song.entity';
 import { User } from 'src/users/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { promiseHooks } from 'v8';
 require('dotenv').config();
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
@@ -16,20 +17,22 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   useFactory: async (
     configService: ConfigService,
   ): Promise<TypeOrmModuleOptions> => {
-    return dataSourceOptions(configService);
+    return dataSourceOptions();
   },
 };
 
-export const dataSourceOptions = (
-  configService: ConfigService,
-): DataSourceOptions => ({
+export const dataSourceOptions = (): DataSourceOptions => ({
   type: 'postgres',
-  host: configService.get<string>('dbHost'),
-  port: parseInt(configService.get<string>('dbPort')),
-  username: configService.get('dbUsername'),
-  password: configService.get('password'),
-  database: configService.get('dbName'),
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User, Playlist, Artist, Song],
   synchronize: true,
-  migrations: ['dist/db/migration/*.js'],
+  migrations: ['dist/db/migrations/*.js'],
 });
+
+console.log('env username', process.env.DB_USERNAME);
+
+export const AppDataSource = new DataSource(dataSourceOptions());
